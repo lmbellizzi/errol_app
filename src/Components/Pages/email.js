69968adf6c0b24600868f082746c6e1bbfb06e15
Leandro Bellizzi <lbellizzi.dev@gmail.com>
2018-04-20 00:47:04 -0300

    Add fetch method to get API json

--- src/Components/Pages/email.js
+++ src/Components/Pages/email.js
@@ -1,18 +1,58 @@
 import React, { Component } from 'react'
-import { Text, StyleSheet, View, StatusBar } from 'react-native'
+import { Text, StyleSheet, View, FlatList, ActivityIndicator } from 'react-native'
 
 export default class MessagesView extends Component {
   constructor(props, context) {
     super(props, context)
+
+    this.state = {
+      isLoading: true
+    }
   }
-  
+    
+  componentDidMount() {
+    return fetch('https://stark-stream-15622.herokuapp.com/messages')
+      .then((resp) => resp.json())
+      .then((respJson) => {
+        this.setState({
+          isLoading: false,
+          dataSource: respJson
+        }, function() {
+
+        })
+      })
+      .catch((error) => {
+        console.error(error)
+      })
+  }
+
   render() {
+    if (this.state.isLoading) {
+      return (
+        <View style={styles.containerLoading}>
+          <ActivityIndicator />
+        </View>
+      )
+    }
     return (
-      <View>
-        <Text> Messages View </Text>
+      <View style={styles.container}>
+        <FlatList
+          data={this.state.dataSource}
+          renderItem={({item}) => <Text>{item.title}</Text>}
+          keyExtractor={(tem, index) => index}
+        />
       </View>
     )
   }
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 20
+  },
+  containerLoading: {
+    flex: 1,
+    padding: 20
+  }
+})
